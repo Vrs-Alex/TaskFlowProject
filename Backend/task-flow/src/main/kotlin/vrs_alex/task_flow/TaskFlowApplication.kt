ac21b9ae9c4660cc725d7a31015fe3859b876d4f
Vrs-Alex <alexgm0508@gmail.com>
2026-02-09 20:08:09 +0300

    init

---
+++ Backend/task-flow/src/main/kotlin/vrs_alex/task_flow/TaskFlowApplication.kt
@@ -0,0 +1,11 @@
+package vrs_alex.task_flow
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class TaskFlowApplication
+
+fun main(args: Array<String>) {
+	runApplication<TaskFlowApplication>(*args)
+}
---
+++ Backend/task-flow/src/test/kotlin/vrs_alex/task_flow/TaskFlowApplicationTests.kt
@@ -0,0 +1,13 @@
+package vrs_alex.task_flow
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class TaskFlowApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
