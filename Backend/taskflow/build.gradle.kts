48b04f7c9121bc2a9eefa95065bb40dada11d5ee
Vrs-Alex <alexgm0508@gmail.com>
2026-02-10 12:00:41 +0300

    added impl in gradle

--- Backend/taskflow/build.gradle.kts
+++ Backend/taskflow/build.gradle.kts
@@ -1,6 +1,10 @@
+import org.jetbrains.kotlin.gradle.dsl.JvmTarget
+
 plugins {
-	kotlin("jvm") version "2.2.21"
-	kotlin("plugin.spring") version "2.2.21"
+	kotlin("jvm") version "2.3.0"
+	kotlin("plugin.spring") version "2.3.0"
+	kotlin("plugin.serialization") version "2.3.0"
+
 	id("org.springframework.boot") version "4.0.2"
 	id("io.spring.dependency-management") version "1.1.7"
 }
@@ -11,7 +15,7 @@ description = "---"
 
 java {
 	toolchain {
-		languageVersion = JavaLanguageVersion.of(24)
+		languageVersion = JavaLanguageVersion.of(25)
 	}
 }
 
@@ -20,6 +24,29 @@ repositories {
 }
 
 dependencies {
+	// WebFlux & Coroutines
+	implementation("org.springframework.boot:spring-boot-starter-webflux")
+	implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core")
+	implementation("org.jetbrains.kotlinx:kotlinx-coroutines-reactor")
+	implementation("org.jetbrains.kotlinx:kotlinx-coroutines-reactive")
+
+	// Database: R2DBC для работы, JDBC для Flyway
+	implementation("org.springframework.boot:spring-boot-starter-data-r2dbc")
+	runtimeOnly("org.postgresql:r2dbc-postgresql")
+	implementation("org.flywaydb:flyway-postgresql") // Flyway
+	runtimeOnly("org.postgresql:postgresql") // JDBC драйвер для Flyway
+
+	// Остальное
+	implementation("org.springframework.boot:spring-boot-starter-validation")
+	implementation("org.springframework.boot:spring-boot-starter-security")
+	implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.7.3")
+
+	// JWT
+	implementation("io.jsonwebtoken:jjwt-api:0.12.6")
+	runtimeOnly("io.jsonwebtoken:jjwt-impl:0.12.6")
+	runtimeOnly("io.jsonwebtoken:jjwt-jackson:0.12.6")
+
+	developmentOnly("org.springframework.boot:spring-boot-devtools")
 	implementation("org.springframework.boot:spring-boot-starter")
 	implementation("org.jetbrains.kotlin:kotlin-reflect")
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
@@ -27,8 +54,13 @@ dependencies {
 	testRuntimeOnly("org.junit.platform:junit-platform-launcher")
 }
 
+tasks.withType<JavaExec> {
+	jvmArgs("--enable-native-access=ALL-UNNAMED")
+}
+
 kotlin {
 	compilerOptions {
+		jvmTarget.set(JvmTarget.JVM_25)
 		freeCompilerArgs.addAll("-Xjsr305=strict", "-Xannotation-default-target=param-property")
 	}
 }
