44dd5dd263e1ddc531418f3950063c3347b559e1
Vrs-Alex <alexgm0508@gmail.com>
2026-04-21 16:41:24 +0300

    init only android app

--- Client/taskflow-client/composeApp/src/commonMain/kotlin/com/vrsalex/taskflow_client/Greeting.kt
+++
@@ -1,9 +0,0 @@
-package com.vrsalex.taskflow_client
-
-class Greeting {
-    private val platform = getPlatform()
-
-    fun greet(): String {
-        return "Hello, ${platform.name}!"
-    }
-}
--- Client/taskflow-client/composeApp/src/iosMain/kotlin/com/vrsalex/taskflow_client/MainViewController.kt
+++
@@ -1,5 +0,0 @@
-package com.vrsalex.taskflow_client
-
-import androidx.compose.ui.window.ComposeUIViewController
-
-fun MainViewController() = ComposeUIViewController { App() }
